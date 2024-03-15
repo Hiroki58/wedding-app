ed9ab163679b284d900b0a01dafbc2f23b9c8e3f
Hiroki58 <gnarly.hiro.15@gmail.com>
2024-03-15 18:28:35 +0900

    added like function

--- src/assets/photo-box.jsx
+++ src/assets/photo-box.jsx
@@ -1,111 +1,45 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Margin } from '@mui/icons-material';
+import { useState } from "react";
 
-export default function TitlebarImageList() {
+
+export default function PhotoBox(props) {
+
+    const [itemData, setItemData] = useState(props.itemData);
+    const [clicked, setCliked] = useState(false);
+
+    const handleLike = (e) => {
+        if (!clicked) setCliked(true);
+        if (clicked) return;
+        console.log(props);
+        e.target.style.color = "red";
+        setItemData(props.itemData.like += 1);
+    }
+
     return (
-        <ImageList sx={{ width: "100vw", height: "70vh" }}>
-            <ImageListItem key="Subheader" cols={2}>
-                <ListSubheader component="div">Photo</ListSubheader>
-            </ImageListItem>
-            {itemData.map((item) => (
-                <ImageListItem key={item.img}>
-                    <img
-                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                        src={`${item.img}?w=248&fit=crop&auto=format`}
-                        alt={item.title}
-                        loading="lazy"
-                    />
-                    <ImageListItemBar
-                        title={item.title}
-                        subtitle={item.author}
-                        actionIcon={
-                            <IconButton aria-label="like" sx={{ mr: 3 }}>
-                                <FavoriteIcon />
-                            </IconButton>
-                        }
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
-    );
+        <ImageListItem key={props.itemData.img}>
+            <img
+                srcSet={`${props.itemData.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${props.itemData.img}?w=248&fit=crop&auto=format`}
+                alt={props.itemData.title}
+                loading="lazy"
+            />
+            <ImageListItemBar
+                title={props.itemData.title}
+                subtitle={props.itemData.author}
+                actionIcon={
+                    <IconButton aria-label="like" onClick={handleLike} sx={{ mr: 3, color: props.color ? "red" : "black" }}>
+                        <FavoriteIcon />
+                        <div className='pointer-events-none'>
+                            {props.itemData.like}
+                        </div>
+                    </IconButton>
+                }
+            />
+        </ImageListItem>
+    )
 }
 
-const itemData = [
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Breakfast',
-        author: '@bkristastucchio',
-        rows: 2,
-        cols: 2,
-        featured: true,
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Burger',
-        author: '@rollelflex_graphy726',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Camera',
-        author: '@helloimnik',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Coffee',
-        author: '@nolanissac',
-        cols: 2,
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Hats',
-        author: '@hjrc33',
-        cols: 2,
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Honey',
-        author: '@arwinneil',
-        rows: 2,
-        cols: 2,
-        featured: true,
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Basketball',
-        author: '@tjdragotta',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Fern',
-        author: '@katie_wasserman',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Mushrooms',
-        author: '@silverdalex',
-        rows: 2,
-        cols: 2,
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Tomato basil',
-        author: '@shelleypauls',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Sea star',
-        author: '@peterlaster',
-    },
-    {
-        img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
-        title: 'Bike',
-        author: '@southside_customs',
-        cols: 2,
-    },
-];
--- src/routes/photo.jsx
+++ src/routes/photo.jsx
@@ -1,13 +1,86 @@
 import BottomNav from "../assets/bottom-nav"
 import { Button, Container, Stack } from '@mui/material'
 import PhotoBox from "../assets/photo-box"
+import ListSubheader from '@mui/material/ListSubheader';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+
 
 export default function Photo() {
+    const itemData = [
+        {
+            id: 1,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Breakfast',
+            author: '@bkristastucchio',
+            like: 5,
+        },
+        {
+            id: 2,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Burger',
+            author: '@rollelflex_graphy726',
+            like: 2,
+        },
+        {
+            id: 3,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Camera',
+            author: '@helloimnik',
+            like: 54,
+
+        },
+        {
+            id: 4,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Coffee',
+            author: '@nolanissac',
+            like: 9,
+        },
+        {
+            id: 5,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Breakfast',
+            author: '@bkristastucchio',
+            like: 5,
+        },
+        {
+            id: 6,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Burger',
+            author: '@rollelflex_graphy726',
+            like: 2,
+        },
+        {
+            id: 7,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Camera',
+            author: '@helloimnik',
+            like: 54,
+
+        },
+        {
+            id: 8,
+            img: 'https://mofusand-mofumofu-market.jp/cdn/shop/files/4970381663407_800x.jpg?v=1686628364',
+            title: 'Coffee',
+            author: '@nolanissac',
+            like: 9,
+        },
+    ];
     return (
         <>
             <Container sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', marginTop: "5px", marginBottom: "5px" }}>
-                <PhotoBox />
+                <ImageList sx={{ width: "100vw", height: "70vh" }}>
+                    <ImageListItem key="Subheader" cols={2}>
+                        <ListSubheader component="div">Photo</ListSubheader>
+                    </ImageListItem>
+                    {itemData.map((item) => (
+                        <PhotoBox itemData={item} />
+                    ))}
+                </ImageList>
             </Container>
+
             <Container>
                 <BottomNav sx={{ width: "100vw" }} />
             </Container>
