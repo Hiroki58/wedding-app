532d54668aecdd0488f631fcff49591bc3a3fdfc
Hiroki58 <gnarly.hiro.15@gmail.com>
2024-03-27 12:22:01 +0900

    spotify

--- src/routes/playlist.jsx
+++ src/routes/playlist.jsx
@@ -17,7 +17,7 @@ export default function Playlist() {
         <div id="playlistContainer" className="flex flex-row flex-wrap justify-evenly w-screen mt-5">
             <AlertDialog title="Wedding music playlist" content="Let's add your favorite song to our wedding playlist!!" />
             <div id="musicPlayer" style={{ width: '600px', height: '80vh' }}>
-                <iframe style={{ borderRadius: 12, width: '600px', height: '100%' }} src="https://open.spotify.com/embed/playlist/26wd56yxtnTqiTpA7jJo7Q?utm_source=generator" loading="lazy"></iframe>
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/26wd56yxtnTqiTpA7jJo7Q?utm_source=generator" width="80%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
             <div id="searchBox" style={{ width: '600px', height: '80vh' }}>
                 <div className="sticky top-0 backdrop-blur-xl">
